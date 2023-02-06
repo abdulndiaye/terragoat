6eb783b98785e293a9ce030f7072a0a74fae5a3d
abdulndiaye <123994208+abdulndiaye@users.noreply.github.com>
2023-02-06 22:15:43 +0000

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "data" {
+resource  "aws_s3_bucket" "data" {
   # bucket is public
   # bucket is not encrypted
   # bucket does not have access logs
@@ -20,7 +20,7 @@ resource "aws_s3_bucket" "data" {
   })
 }
 
-resource "aws_s3_bucket_object" "data_object" {
+resource  "aws_s3_bucket_object" "data_object" {
   bucket = aws_s3_bucket.data.id
   key    = "customer-master.xlsx"
   source = "resources/customer-master.xlsx"
@@ -39,7 +39,7 @@ resource "aws_s3_bucket_object" "data_object" {
   })
 }
 
-resource "aws_s3_bucket" "financials" {
+resource  "aws_s3_bucket" "financials" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
@@ -62,7 +62,7 @@ resource "aws_s3_bucket" "financials" {
 
 }
 
-resource "aws_s3_bucket" "operations" {
+resource  "aws_s3_bucket" "operations" {
   # bucket is not encrypted
   # bucket does not have access logs
   bucket = "${local.resource_prefix.value}-operations"
